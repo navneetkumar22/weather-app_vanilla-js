d695ca2a7c14b8713fcc15dc676f603ded9cb175
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-07-21 20:01:45 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Weather Application - Vanilla JS
+This is a weather application built using HTML, CSS and Vanilla JavaScript.
+- It fetch data from open-weather api.
+
+<br/>
+
+## Screenshot 
+![screenshot1](./screenshot.png)
---
+++ script.js
@@ -0,0 +1,26 @@
+let apiKey = "0d94c6acf027134d94d41107d7c1bd2a";
+let searchInput = document.getElementById('seachInput');
+let myLocation = searchInput.value;
+
+let getWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${myLocation}&units=metric&appid=${apiKey}`)
+        .then(res => { return res.json() })
+        .then(result => { console.log(result); })
+        .catch(err => { console.log(err); })
+}
+
+
+let getDaily = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${myLocation}&cnt=6&units=metric&appid=${apiKey}`)
+        .then(res => { return res.json() })
+        .then(result => { console.log(result.list); })
+        .catch(err => { console.log(err); })
+}
+
+
+
+const getSearch = () => {
+    console.log(searchInput.value);
+    getWeather();
+    getDaily();
+}
